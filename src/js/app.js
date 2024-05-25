9279d515f20d7a20344b1ac77f74d14c22d1e619
HigorMenz <higormenzinger1997@outlook.com>
2024-05-25 16:38:56 -0400

    dark mode added and fixed mobile menu bugs

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     eventListeners();
+    darkMode({})
 
 
 });
@@ -22,4 +23,30 @@ function responsiveNavigation(){
         navbar.classList.add('show');
     } */
 
+}
+
+function darkMode (){
+
+    const  userThemeDefault = window.matchMedia('(prefers-color-scheme: dark)');
+    
+    console.log("darkmode");
+
+    if(userThemeDefault.matches){
+        document.body.classList.add('dark-mode');
+    }else{
+        document.body.classList.remove('dark-mode')
+    }
+
+    userThemeDefault.addEventListener('change', function(){
+        if(userThemeDefault.matches){
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode')
+        }
+    })
+
+    const darkMode = document.querySelector('.dark-mode-button')
+    darkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode')
+    });
 }
