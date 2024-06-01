ed814113e695292f5b6daa7af8edb7bc09583ff9
HigorMenz <higormenzinger1997@outlook.com>
2024-06-01 14:25:17 -0400

    register properties in the database

--- admin/realestates/create.php
+++ admin/realestates/create.php
@@ -7,53 +7,71 @@ $db = connectDB();
 //form validation
 $err = [];
 
+//get brokers
+$brokers= "SELECT * FROM brokers";
+$res = mysqli_query($db,$brokers);
+//
+$title  ="";
+$price ="";
+$description ="";
+$bedroom ="";
+$bathroom ="";
+$parking ="";
+$brokerId ="";
+
 
 //insert info in bd
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  echo "<pre>";
-  var_dump($_POST);
-  echo "</pre>";
+  //echo "<pre>";
+  //v0ar_dump($_POST);
+  //echo "</pre>";
 
-  $title  = $_POST['title'];
-  $price = $_POST['price'];
-  $description = $_POST['description'];
-  $bedroom = $_POST['bedroom'];
-  $bathroom = $_POST['bathroom'];
-  $parking = $_POST['parking'];
-  $brokerId = $_POST['brokers'];
+  $title  = mysqli_real_escape_string($db, $_POST['title']);
+  $price = mysqli_real_escape_string($db, $_POST['price']);
+  $description = mysqli_real_escape_string($db, $_POST['description']);
+  $bedroom = mysqli_real_escape_string($db, $_POST['bedroom']);
+  $bathroom = mysqli_real_escape_string($db, $_POST['bathroom']);
+  $parking = mysqli_real_escape_string($db, $_POST['parking']);
+  $brokerId = mysqli_real_escape_string($db, $_POST['brokers']);
+  $created = date('Y/m/d');
 
 
-  if(!$title){
+  if (!$title) {
     $err[] = "You need to insert a title.";
   }
-  if(!$price){
+  if (!$price) {
     $err[] = "You need to insert a price.";
   }
-  if(strlen($description) < 50){
+  if (strlen($description) < 50) {
     $err[] = "You need to insert a description of at least 50 characters.";
   }
-  if(!$bedroom){
+  if (!$bedroom) {
     $err[] = "The number of bedrooms is required.";
   }
-  if(!$bathroom){
+  if (!$bathroom) {
     $err[] = "The number of bathrooms is required.";
   }
-  if(!$parking){
+  if (!$parking) {
     $err[] = "The number of parking slots is required.";
   }
-  if(!$brokerId){
+  if (!$brokerId) {
     $err[] = "You need to select a broker.";
   }
 
 
-  //insert in to bd
-  $query = " INSERT INTO realestates (title, price, description, bedrooms, bathrooms, parkingslots, brokers_id ) VALUES ('$title','$price','$description','$bedroom','$bathroom','$parking','$brokerId')";
+  //Review if the array of errors is empty.
 
-  echo $query;
+  if (empty($err)) {
+    //insert on bd
+    $query = " INSERT INTO realestates (title, price, description, bedrooms, bathrooms, parkingslots,created, brokers_id ) VALUES ('$title','$price','$description','$bedroom','$bathroom','$parking', ' $created', '$brokerId')";
 
-  $result = mysqli_query($db, $query);
-  if ($result) {
-    echo "inserted successfully";
+    $result = mysqli_query($db, $query);
+
+    if ($result) {
+      //Redirect
+     
+      header('Location: /ihouse/admin/index.php');
+    }
   }
 }
 
@@ -69,20 +87,28 @@ templateInclude('header');
   <h1>create</h1>
   <a href="/ihouse/admin/index.php" class="button green-button"> Return </a>
 
+  <?php foreach ($err as $error) : ?>
+    <div class="alert error">
+
+      <?php echo $error; ?>
+    </div>
+
+  <?php endforeach; ?>
+
   <form class="form" method="POST" action="../../admin/realestates/create.php">
     <fieldset>
       <legend>General info</legend>
       <label for="title">Title:</label>
-      <input type="text" id="title" name="title" placeholder="Title">
+      <input type="text" id="title" name="title" placeholder="Title" value="<?php echo $title; ?>">
 
       <label for="price">Price:</label>
-      <input type="number" id="price" name="price" placeholder="Price">
+      <input type="number" id="price" name="price" placeholder="Price" value="<?php echo $price; ?>">
 
       <label for="image">Image:</label>
       <input type="file" id="image" name="image" accept="image/jpeg, image/png">
 
       <label for="description">Description:</label>
-      <textarea name="description" name="description" id="description"></textarea>
+      <textarea name="description" name="description" id="description" value="<?php echo $description; ?>"></textarea>
 
     </fieldset>
 
@@ -90,13 +116,13 @@ templateInclude('header');
       <legend>Property Information</legend>
 
       <label for="bedroom">Bedrooms:</label>
-      <input type="number" id="bedroom" name="bedroom" placeholder="E.g: 5" min="1" max="9">
+      <input type="number" id="bedroom" name="bedroom" placeholder="E.g: 5" min="1" max="9" value="<?php echo $bedroom; ?>">
 
       <label for="bathroom">Bathrooms:</label>
-      <input type="number" id="bathroom" name="bathroom" placeholder="E.g: 2" min="1" max="9">
+      <input type="number" id="bathroom" name="bathroom" placeholder="E.g: 2" min="1" max="9" value="<?php echo $bathroom; ?>">
 
       <label for="parking">Parking Slots:</label>
-      <input type="number" id="parking" name="parking" placeholder="E.g: 4" min="1" max="9">
+      <input type="number" id="parking" name="parking" placeholder="E.g: 4" min="1" max="9" value="<?php echo $parking; ?>">
 
 
     </fieldset>
@@ -104,10 +130,11 @@ templateInclude('header');
     <fieldset>
       <legend>Broker</legend>
 
-      <select name="brokers">
+      <select name="brokers" value="">
         <option value="" disabled selected>--- Select --- </option>
-        <option value="1">Higor</option>
-        <option value="2">Jhon</option>
+        <?php while($row = mysqli_fetch_assoc($res)) : ?>
+          <option <?php echo $brokerId === $row['id'] ? 'selected' : ""; ?> value="<?php echo $row['id']; ?>"> <?php echo $row['name'] ." ". $row['surname']; ?></option>
+          <?php endwhile; ?>
       </select>
     </fieldset>
 
